ebda374b0ce4f96a6188a387110089f2b398a3fa
ArnaudManuel <arnaud.manuel@netplus.ch>
2018-11-10 00:13:45 +0100

    first push

---
+++ middleware/mauris-mdw/src/main/java/io/swagger/MaurisMdwApplication.java
@@ -0,0 +1,12 @@
+package io.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaurisMdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaurisMdwApplication.class, args);
+	}
+}
---
+++ middleware/mauris-mdw/src/main/java/io/swagger/persistence/VirtualPOD.java
@@ -0,0 +1,18 @@
+package io.swagger.persistence;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class VirtualPOD {
+
+	@Id
+	String podName;
+	
+	List<String> content;
+}
