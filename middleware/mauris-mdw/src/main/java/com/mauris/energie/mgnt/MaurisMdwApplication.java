04e95da42c2f1857ae4fb6d35d1297575541c997
JMauris <j.mauris@hotmail.fr>
2018-11-10 14:12:36 +0100

    Packaging war

--- middleware/mauris-mdw/src/main/java/com/mauris/energie/mgnt/MaurisMdwApplication.java
+++ middleware/mauris-mdw/src/main/java/com/mauris/energie/mgnt/MaurisMdwApplication.java
@@ -11,28 +11,23 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
-public class MaurisMdwApplication {
+public class MaurisMdwApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MaurisMdwApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaurisMdwApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner restTest(RestAccess test) {
-		return (arg)->{
-			EventTemplate data = test.getAmbrosus("CH1014001234500000000000000006860_test", new Date(1L), new Date());
-            System.out.println(data.toString());
-
-			History history = TemplateConverter.toHistory(data);
-
-			System.out.println(history.toString());
-		};
-	}
-
 }
+
