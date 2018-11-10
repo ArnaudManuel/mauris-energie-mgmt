ca36427f460bd2eb3d68308323ecf96a1a076881
JMauris <j.mauris@hotmail.fr>
2018-11-10 02:54:44 +0100

    fix bug illegale "i"

--- middleware/mauris-mdw/src/main/java/com/mauris/energie/mgnt/MaurisMdwApplication.java
+++ middleware/mauris-mdw/src/main/java/com/mauris/energie/mgnt/MaurisMdwApplication.java
@@ -2,7 +2,10 @@ package com.mauris.energie.mgnt;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import com.mauris.energie.mgnt.ambrosusTemplate.RestAccess;
@@ -11,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class MaurisMdwApplication {
 
 	public static void main(String[] args) {
